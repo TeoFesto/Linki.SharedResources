f784e32a97a7ef618c34836a00232a16471538c6
Bogdan Degtiarenko <teofesto1523@gmail.com>
2023-02-14 22:00:46 +0400

    add ServerConnectionResponse

---
+++ ServerConnectionResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linki.SharedResources
+{
+    public class ServerConnectionResponse : Response
+    {
+        public ServerConnectionResponse() { }
+        public ServerConnectionResponse(IPEndPoint endPoint)
+        {
+            ipAddress = endPoint.Address.ToString();
+            port = endPoint.Port;
+        }
+        public string ipAddress { get; set; }
+        public int port { get; set; }
+    }
+}
