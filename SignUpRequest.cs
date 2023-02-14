6bdf831c47c7d149ededa57f19a84c4642706dad
Bogdan Degtiarenko <teofesto1523@gmail.com>
2023-02-14 21:59:36 +0400

    add SignUpRequest AND add SignUpResponse

---
+++ SignUpRequest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Linki.SharedResources
+{
+    public class SignUpRequest : Request
+    {
+        [Required(ErrorMessage = "Поле логина не должно быть пустым")]
+        public string Login { get; set; }
+
+
+        [Required(ErrorMessage = "Поле никнейма не должно быть пустым")]
+        public string Nickname { get; set; }
+
+
+        [Required(ErrorMessage = "Поле e-mail адреса не должно быть пустым")]
+        [EmailAddress(ErrorMessage = "Неправильный формат e-mail адреса")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Поле пароля не должно быть пустым")]
+        public string Password { get; set; }
+
+
+        [Required(ErrorMessage = "Поле повтора пароля не должно быть пустым")]
+        [Compare("Password", ErrorMessage = "Повторённый пароль должен совпадать с оригинальным")]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ SignUpResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linki.SharedResources
+{
+    public class SignUpResponse : Response
+    {
+        public bool isSignedUp { get; set; }
+        public string StatusMessage { get; set; } 
+    }
+}
